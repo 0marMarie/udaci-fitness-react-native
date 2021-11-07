0996502fc203401fe1cddbdf0c738d673e05e545
Omar Marie <omarhamed440@gmail.com>
2021-11-07 11:13:31 +0200

    ⚙ Initial Commit

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
-import * as React from "react";
-import { View, Text } from "react-native";
+import React, {Component} from "react"
+import { View, Text } from "react-native"
+import AddEntry from "./components/AddEntry"
+import { getMetricMetaInfo } from "./utils/helpers"
 
-export default function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
-  );
+export default class App extends Component {
+  componentDidMount() {
+    console.log('f u')
+  }
+  render(){
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <AddEntry/>
+      </View >
+    )
+  }
 }
