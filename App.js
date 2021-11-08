99bbe4de0e2a2027b841744ec6db1b947f819a77
Omar Marie <omarhamed440@gmail.com>
2021-11-08 02:49:57 +0200

    🤙 add redux to our app

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
-import React, {Component} from "react"
-import { View, Text } from "react-native"
-import AddEntry from "./components/AddEntry"
-import { getMetricMetaInfo } from "./utils/helpers"
+import React from 'react'
+import { View } from 'react-native'
+import AddEntry from './components/AddEntry'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
 
-export default class App extends Component {
-  componentDidMount() {
-    console.log('Started Successfully! OMAR~MARIE')
-  }
-  render(){
+export default class App extends React.Component {
+  render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <AddEntry/>
-      </View >
+      <Provider store={createStore(reducer)}>
+        <View>
+          <AddEntry />
+        </View>
+      </Provider>
     )
   }
 }
